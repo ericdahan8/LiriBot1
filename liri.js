49918bb2bb4fa253ef7bcd272b61669cc570349b
Eric Dahan <Ericdahan@dyn-160-39-244-68.dyn.columbia.edu>
2019-03-21 18:02:07 -0400

    Added Movies

--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
 var dataArr;
 ////command into process afgv
-var command = process.argv[2];
+var inputs = process.argv[2];
 /////////////////////////////////
 //process the additonal arguments with a space e.g. post malone
 var arguments = [];
@@ -18,80 +18,57 @@ for (var i = 3; i < process.argv.length; i++) {
   arguments.push(process.argv[i]);
 }
 
-var argString = arguments.join(" ");
+var Str = arguments.join(" ");
 ///////////////////////////////////////////////////////////////////////////////////////////
 ///Change order and command name and arg string and spotifythissong
-if (command === "spotify-") {
-  spotifyThisSong(argString);
-} else if (command === "concert-") {
-  concertThis(argString);
-} else if (command === "movie-this") {
-  movieThis(argString);
-} else if (command === "do-what-it-says") {
+if (inputs === "spotify-") {
+  spotifySong(Str);
+} else if (inputs === "concert-") {
+  concert(Str);
+} else if (inputs === "movie-") {
+  movie(Str);
+} else if (inputs === "do-what-it-says") {
   doWhatItSays();
 } else {
   console.log("Invalid Input");
 }
-
-function spotifyThisSong(arg) {
-  if (arguments.length < 1) {
-    /////////////default tracks
-    spotify.search({ type: "track", query: "All the small things" }, function(
-      err,
-      data
-    ) {
-      if (err) {
-        return console.log("Error occurred: " + err);
-      }
-      console.log("No song - default track");
-      console.log("Song:", data.tracks.items[0].name);
-      console.log("Artist(s):", data.tracks.items[0].artists[0].name);
+////////////////////////////////////Spotify
+function spotifySong(arg) {
+  spotify.search({ type: "track", limit: 1, query: arg }, function(err, data) {
+    if (err) {
+      return console.log("Error: " + err);
+    }
+    ////////////requested tracks
+    for (var i = 0; i < data.tracks.items.length; i++) {
+      console.log(data.tracks.items[i].artists[0].name);
+      console.log("Song:", data.tracks.items[i].name);
+      console.log("Artist(s):", data.tracks.items[i].artists[0].name);
       if (data.tracks.items[i].preview_url === null) {
-        console.log("No Preview URL");
+        console.log("No Preview Found");
       } else {
-        console.log("Preview URL:", data.tracks.items[0].preview_url);
+        console.log("Preview URL:", data.tracks.items[i].preview_url);
       }
-      console.log("Album:", data.tracks.items[0].album.name);
-    });
-  } else {
-    spotify.search({ type: "track", limit: 1, query: arg }, function(
-      err,
-      data
-    ) {
-      if (err) {
-        return console.log("Error: " + err);
-      }
-      ////////////requested tracks
-      for (var i = 0; i < data.tracks.items.length; i++) {
-        console.log(data.tracks.items[i].artists[0].name);
-        console.log("Song:", data.tracks.items[i].name);
-        console.log("Artist(s):", data.tracks.items[i].artists[0].name);
-        if (data.tracks.items[i].preview_url === null) {
-          console.log("No Preview URL");
-        } else {
-          console.log("Preview URL:", data.tracks.items[i].preview_url);
-        }
-        console.log("Album:", data.tracks.items[i].album.name);
-      }
-    });
-  }
+      console.log("Album:", data.tracks.items[i].album.name);
+    }
+  });
 }
+
 ///////////////////////Concert This
 
-function concertThis(arguments) {
+function concert(arguments) {
   if (arguments.length < 1) {
     return console.log("No artist, Try Again.");
   }
   axios
     .get(
       "https://rest.bandsintown.com/artists/" +
-        argString.toString() +
+        Str.toString() +
         "/events?app_id=codingbootcamp"
     )
     .then(function(response) {
-      console.log("Here are the events for " + argString);
+      console.log("Here are the events for " + Str);
       response.data.forEach(function(event) {
-        console.log(argString);
+        console.log(Str);
         console.log("Venue:", event.venue.name);
         console.log("Location:", event.venue.city + " " + event.venue.region);
         console.log(
@@ -101,4 +78,26 @@ function concertThis(arguments) {
       });
     });
 }
-///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+//Movie This funciton
+function movie(arguments) {
+  if (arguments === undefined || arguments.length < 1) {
+    return console.log(
+      "Deafult Movie: 'Mr. Nobody,' - http://www.imdb.com/title/tt0485947/"
+    );
+  }
+  axios
+    .get(
+      "http://www.omdbapi.com/?t=" + arguments + "&y=&plot=short&apikey=trilogy"
+    )
+    .then(function(response) {
+      console.log("Title:", response.data.Title);
+      console.log("Year:", response.data.Year);
+      console.log("IMDB Rating:", response.data.imdbRating);
+      console.log("Metascore Rating:", response.data.Metascore + "%");
+      console.log("Country:", response.data.Country);
+      console.log("Language(s):", response.data.Language);
+      console.log("Plot:", response.data.Plot);
+      console.log("Actors:", response.data.Actors);
+    });
+}
